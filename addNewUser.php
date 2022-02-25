4d2a6a1acc3bb9cc872c9f78396ab48aa2d7ec9d
ismail belgacem <47541827+ismail998@users.noreply.github.com>
2022-02-25 19:46:03 +0100

    init

--- addNewUser.php
+++ addNewUser.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
                 <div class="d-flex align-items-center">
                     <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-2 m-0">Settings</h2>
+                    <h2 class="fs-2 m-0">add new admin</h2>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
